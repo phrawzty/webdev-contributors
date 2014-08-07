7ac9307fefbcc2e8d9ac7b56074d3255b8252dd9
phrawzty <phrawzty+github@gmail.com>
2014-08-07 18:10:00 +0200

    github OAuth token support

--- get_contributors.py
+++ get_contributors.py
@@ -13,6 +13,8 @@ from os.path import dirname
 # See also: https://developer.github.com/v3/#rate-limiting
 GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', None)
 GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', None)
+GITHUB_OAUTH_TOKEN = os.getenv('GITHUB_OAUTH_TOKEN', None)
+
 GITHUB_EMAIL_CACHE_AGE = int(os.getenv('GITHUB_EMAIL_CACHE_AGE',
                                        60 * 60 * 24 * 7))
 GITHUB_REPOS_CACHE_AGE = int(os.getenv('GITHUB_REPOS_CACHE_AGE',
@@ -83,8 +85,14 @@ def main():
 
         repocontributors = github_api_get('%s/contributors' % repo, None,
                                    'repocontributors', GITHUB_REPOS_CACHE_AGE)
+
         for repocontributor in repocontributors:
-            username = repocontributor['login']
+            # Catch and display an unexpected API response.
+            try:
+                username = repocontributor['login']
+            except TypeError:
+                print repocontributors
+                exit(1)
             contributions = repocontributor['contributions']
             contributor = contributors.setdefault(username, {
                 'username': username, 'email': None
@@ -143,6 +151,7 @@ def print_contributors_by_level(contributors):
 def github_api_url(url, params=None):
     """Append the GitHub client details, if available"""
     url = '%s/%s' % (GITHUB_BASE_URL, url)
+
     if not params:
         params = {}
     if GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET:
@@ -152,6 +161,7 @@ def github_api_url(url, params=None):
         ))
     if params:
         url = '%s?%s' % (url, urllib.urlencode(params))
+
     return url
 
 
@@ -159,30 +169,35 @@ def github_api_get(path, params=None, cache_name=False, cache_timeout=3600):
     """Cached HTTP GET to GitHub repos API"""
     url = github_api_url(path, params)
 
-    # If no cache name, then cache is disabled.
-    if not cache_name:
-        return requests.get(url).json()
-
-    # Build a cache path based on MD5 of URL
-    path_hash = hashlib.md5(url).hexdigest()
-    cache_path = CACHE_PATH_TMPL % (cache_name, path_hash)
-
-    # Create the cache path, if necessary
-    cache_dir = dirname(cache_path)
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir)
-
-    # Attempt to load up data from cache
     data = None
-    if os.path.exists(cache_path) and file_age(cache_path) < cache_timeout:
-        try:
-            data = json.load(open(cache_path, 'r'))
-        except ValueError:
-            pass
+
+    if cache_name:
+
+        # Build a cache path based on MD5 of URL
+        path_hash = hashlib.md5(url).hexdigest()
+        cache_path = CACHE_PATH_TMPL % (cache_name, path_hash)
+
+        # Create the cache path, if necessary
+        cache_dir = dirname(cache_path)
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        # Attempt to load up data from cache
+        data = None
+        if os.path.exists(cache_path) and file_age(cache_path) < cache_timeout:
+            try:
+                data = json.load(open(cache_path, 'r'))
+            except ValueError:
+                pass
 
     # If data was missing or stale from cache, finally perform GET
     if not data:
-        data = requests.get(url).json()
+        if GITHUB_OAUTH_TOKEN:
+            data = requests.get(url, auth=(GITHUB_OAUTH_TOKEN,'x-oauth-basic')).json
+        else:
+            data = requests.get(url).json
+
+    if cache_name:
         json.dump(data, open(cache_path, 'w'))
 
     return data
